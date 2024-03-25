7b4e8adced0e757279eb186effb5128ef133ca7a
yinka0136 <yinka0136@gmail.com>
2024-03-25 12:46:38 +0100

    commit

--- src/app/components/join/join.component.ts
+++ src/app/components/join/join.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class JoinComponent implements OnInit {
+  @ViewChild('closeModalButton')
+  closeModalButton!: ElementRef<HTMLButtonElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        'service_9dql108',
+        'template_i3m516c',
+        e.target as HTMLFormElement,
+        'LIhYj7NSHmNSnZBfD'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          this.closeModalButton.nativeElement.click();
+          alert('Email sent successfully');
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   }
-
 }
